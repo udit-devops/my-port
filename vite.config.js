e2179645d59801fb61c7b9e81db7bc67862ee773
udit-devops <dakuaudit@gmail.com>
2024-10-20 10:48:24 +0530

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  build: {
+    outDir: 'build',  // Ensures the build output goes to the 'build' folder
+  },
+  base: '/my-port/',  // Add this line
+});
